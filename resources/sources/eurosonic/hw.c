2087c33f66a8805f778b615e89b38ee2267e8c5d
Stefanovski <norbert.stefanovski@googlemail.com>
2025-12-05 12:48:24 +0100

    Projekt wird jetzt komplett korrekt erstellt bis zu OpenPLC.bin. Es fehlt noch das MD5 Handling.

--- resources/sources/eurosonic/hw.c
+++ resources/sources/eurosonic/hw.c
@@ -1,14 +1,11 @@
 #include <HAL.h>
 #include <string.h>
 #include "config.h"
+#include "plc_main.h"
+#include "iec_types_all.h"
 
 #include "hw.h"
-
-
-extern uint16_t QW[];
-extern uint16_t IW[];
-extern uint8_t QX[];
-extern uint8_t IX[];
+#include "openplc.h"
 
 //-----------------------------------------------------------------------------
 // Collect all inputs and update the internal buffer IX that points to the 
@@ -20,6 +17,7 @@ void update_inputs(void)
 	
 	// For Testing purposes we will read the inputs from the GPIOs
 	IX[0] = (HAL_GPIO_ReadPin(GPIOI, GPIO_PIN_12) > 0) ? IX[0] | 0x01 : IX[0] & 0xFE;
+	*bool_input[0][0] = IX[0];
 }
 
 //-----------------------------------------------------------------------------
@@ -31,6 +29,7 @@ void update_outputs(void)
 	// In the original generator this writes all outputs to the FPGA (Memory)
 
 	// For Testing purposes we will write the outputs to the GPIOs
+	QX[0] = *bool_output[0][0];
 	HAL_GPIO_WritePin(GPIOI, GPIO_PIN_13, QX[0] & 0x01);
 }
 
