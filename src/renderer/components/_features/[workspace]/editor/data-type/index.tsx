035705048ae68ec2505334b4a0caf2dd47caf82e
Stefanovski <norbert.stefanovski@googlemail.com>
2026-08-23 04:43:57 +0200

    fix(editor): stabilize array data type editing

--- src/renderer/components/_features/[workspace]/editor/data-type/index.tsx
+++ src/renderer/components/_features/[workspace]/editor/data-type/index.tsx
@@ -2,25 +2,15 @@ import { ArrayDataType } from '@root/renderer/components/_molecules/data-types/a
 import { EnumeratorDataType } from '@root/renderer/components/_molecules/data-types/enumerated'
 import { StructureDataType } from '@root/renderer/components/_molecules/data-types/structure'
 import { datatypeSelectors } from '@root/renderer/hooks'
-import { PLCDataType } from '@root/types/PLC/open-plc'
-import { ComponentPropsWithoutRef, useEffect, useState } from 'react'
+import { ComponentPropsWithoutRef } from 'react'
 
 type DatatypeEditorProps = ComponentPropsWithoutRef<'div'> & {
   dataTypeName: string
 }
 
 const DataTypeEditor = ({ dataTypeName, ...rest }: DatatypeEditorProps) => {
-  const [editorContent, setEditorContent] = useState<PLCDataType>()
-
   const dataTypes = datatypeSelectors.useDatatypes()
-
-  useEffect(() => {
-    const dataTypeIndex = dataTypes.findIndex((dataType) => dataType.name === dataTypeName)
-    if (dataTypeIndex !== -1) {
-      const dataType = dataTypes[dataTypeIndex]
-      setEditorContent(dataType)
-    }
-  }, [dataTypes, dataTypeName])
+  const editorContent = dataTypes.find((dataType) => dataType.name === dataTypeName)
 
   return (
     <div aria-label='Data type editor container' className='flex h-full w-full flex-col gap-4' {...rest}>
